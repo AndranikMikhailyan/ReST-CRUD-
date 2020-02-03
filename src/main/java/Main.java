e56ff5e672cb4233d9268cdc36349f647e39ed6d
AndranikMikhailyan <andranik.stav@mail.ru>
2020-02-03 20:50:12 +0300

    На проверку

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import edu.java.controller.rest.CustomerRestController;
 import edu.java.controller.rest.ProjectRestController;
 import edu.java.model.Project;
 
@@ -7,36 +8,12 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        ProjectRestController projectRestController = new ProjectRestController();
+        CustomerRestController controller = new CustomerRestController();
 
-        List<Project> allProject = projectRestController.getAllProject();
+//        Response customer = controller.getCustomer(1L);
 
-        for (Project project : allProject) {
-            System.out.println(project.getId());
-            System.out.println(project.getName());
-            System.out.println(project.getBadget());
-            System.out.println(project.getTeams());
-            System.out.println(project.getCustomer().getId());
-//        }
 
-            ProjectRestController prc = new ProjectRestController();
 
-//        List<Project> allProject1 = prc.getAllProject();
-//        for (Project project : allProject1) {
-//            System.out.println(project.getId());
-//            System.out.println(project.getName());
-//            System.out.println(project.getBadget());
-//            System.out.println(project.getTeams());
-//            System.out.println(project.getCustomer().getId());
-//            project.getCustomer().getProjects().forEach(project1 -> System.out.println(project1.getId()));
-//        }
-//        Response response = prc.getProject(4L);
-//        Project project = (Project) response.getEntity();
-//        System.out.println(project.getId());
-//        System.out.println(project.getName());
-//        System.out.println(project.getBadget());
-//        System.out.println(project.getTeams());
-//        System.out.println(project.getCustomer().getId());
-        }
+
     }
 }
