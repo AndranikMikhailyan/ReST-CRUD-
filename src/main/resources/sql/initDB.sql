6f71c5ff31809f07e384ded51f68b7b08baaf5de
AndranikMikhailyan <andranik.stav@mail.ru>
2020-02-08 16:01:17 +0300

    Добавил liquibase

--- src/main/resources/sql/initDB.sql
+++ src/main/resources/sql/initDB.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS users_skills;
-DROP TABLE IF EXISTS usersId;
-DROP TABLE IF EXISTS skillsId;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS skills;
 DROP TABLE IF EXISTS teams_projects;
-DROP TABLE IF EXISTS teamsId;
-DROP TABLE IF EXISTS projectsId;
+DROP TABLE IF EXISTS teams;
+DROP TABLE IF EXISTS projects;
 DROP TABLE IF EXISTS customers;
 
 CREATE TABLE customers(
@@ -11,7 +11,7 @@ CREATE TABLE customers(
   name VARCHAR(256) NOT NULL
 );
 
-CREATE TABLE projectsId(
+CREATE TABLE projects(
   id INTEGER auto_increment PRIMARY KEY,
   name VARCHAR(256) NOT NULL,
   badget REAL NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE projectsId(
   FOREIGN KEY (customer_id) REFERENCES customers(id)
 );
 
-CREATE TABLE teamsId(
+CREATE TABLE teams(
   id INTEGER auto_increment PRIMARY KEY,
   name VARCHAR(256) NOT NULL
 );
@@ -27,29 +27,29 @@ CREATE TABLE teamsId(
 CREATE TABLE teams_projects(
   team_id INTEGER,
   project_id INTEGER,
-  FOREIGN KEY (team_id) REFERENCES teamsId(id),
-  FOREIGN KEY (project_id) REFERENCES projectsId(id),
+  FOREIGN KEY (team_id) REFERENCES teams(id),
+  FOREIGN KEY (project_id) REFERENCES projects(id),
   UNIQUE (team_id, project_id)
 );
 
-CREATE TABLE skillsId(
+CREATE TABLE skills(
   id INTEGER auto_increment PRIMARY KEY,
   name VARCHAR(256) NOT NULL
 );
 
-CREATE TABLE usersId(
+CREATE TABLE users(
   id INTEGER auto_increment PRIMARY KEY,
   first_name VARCHAR(256) NOT NULL,
   last_name VARCHAR(256) NOT NULL,
   specialty VARCHAR(256) NOT NULL,
   team_id INTEGER,
-  FOREIGN KEY (team_id) REFERENCES usersId(id)
+  FOREIGN KEY (team_id) REFERENCES users(id)
 );
 
 CREATE TABLE users_skills(
   user_id INTEGER NOT NULL,
   skill_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES usersId(id),
-  FOREIGN KEY (skill_id) REFERENCES skillsId(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (skill_id) REFERENCES skills(id),
   UNIQUE (user_id, skill_id)
 );
